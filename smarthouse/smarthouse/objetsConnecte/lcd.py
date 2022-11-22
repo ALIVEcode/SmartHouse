f0fa94f3734c9dad01629043366deec218efd6e9
Francis Mathieu-Gosselin <e2074217@cmaisonneuve.qc.ca>
2022-11-22 12:05:31 -0500

    REM - removed directory maison & lcd now prints 2 lines

--- smarthouse/smarthouse/objetsConnecte/lcd.py
+++ smarthouse/smarthouse/objetsConnecte/lcd.py
@@ -1,14 +1,15 @@
 from alimata.core.board import Board
 from alimata.actuators.lcd import Lcd
-from time import sleep
 
 
 class LCD:
     def __init__(self, board: Board) -> None:
         self.__lcd = Lcd(board=board, adress=0x27, rows=4, cols=20)
 
-    def afficher(self, ligne1: str, ligne2: str, ligne3: str, ligne4: str):
-        self.__lcd.quick_print(ligne1, ligne2, ligne3, ligne4)
-        sleep(1)
 
-
+    def afficher(self, ligne1: str, ligne2: str = ""):
+        '''Quickly prints 1 to 2 lines on the LCD'''
+        self.home()
+        self.print(ligne1)
+        self.set_cursor(0, 1)
+        self.print(ligne2)
